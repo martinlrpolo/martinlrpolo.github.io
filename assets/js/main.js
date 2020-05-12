9b29490e601a93b9838207310b068dc48efad2c3
Martin Leonidas Rodriguez Polo <martin97polo@gmail.com>
2020-05-11 22:56:03 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    // PRELOADER
+    $('#preloader').delay(500).fadeOut('slow'); // will fade out the white DIV that covers the website.
+
+
+        // PAGE LOADER
+    jQuery('#grid-container').on('initComplete.cbp', function () {
+        if ($('#ajax-tab-container').length) {
+            $('#ajax-tab-container').easytabs({
+                tabs: 'header nav ul li',
+                animate			: true,
+                updateHash		: false,
+                transitionIn	:'fadeIn',
+                transitionOut	:'fadeOut',
+                animationSpeed	:100,
+                tabActiveClass	:'active',
+                transitionInEasing: 'linear',
+                  transitionOutEasing: 'linear',
+            });
+        }
+    });
+
+
+    // RESPONSIVE MENU
+    function transform(){
+        var outdiv = '<div class="menuout"><div class="menuin"><ul class="tabs"></ul></div></div>';
+        $(outdiv).appendTo("nav");
+        var resmenus = $('.tabs').html();
+        $(".menuout .menuin .tabs").append(resmenus);
+       $('.menuin').hide(); 
+    }
+    transform();
+    $('.hamburger').on('click', function() {
+       $('.menuin').slideToggle(); 
+    });
+    $('.menuout').on('click', function () {
+        $('.menuin').slideUp();  
+    });
+
+    // OWL CAROUSEL GENERAL JS
+    if ($('.owl-carousel').length) {
+        $('.owl-carousel').each(function () {
+            $(this).owlCarousel({
+                items: $(this).data('items') ? $(this).data('items') : 3
+                , autoPlay: $(this).data('autoplay') ? $(this).data('autoplay') : 2500
+                , pagination: $(this).data('pagination') ? $(this).data('pagination') : false
+                , itemsDesktop: [1199, 3]
+                , itemsDesktopSmall: [979, 3]
+            });
+        });
+    }
+
+
+}); // window load end 
+
+
